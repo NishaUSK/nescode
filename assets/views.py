f8eec0793811cb822ccbaacef515be5728140dad
Nisha <nisha@nescode.com>
2018-11-22 21:27:20 +0530

    [Update]Template

--- assets/views.py
+++ assets/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Electronicgadgets
+from .models import Kitchen
+from .models import Furnitures
+from .models import Plasticmaterials
+from .models import Cars
 
 def post_list(request):
+    Electronicgadgets.objects.filter(launch_date__lte=timezone.now()).order_by('launch_date')
     return render(request, 'assets/post_list.html', {})
