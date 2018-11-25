a8f3de14fdb7a7fb871cd89c17b1efc46b56507d
Nisha <nisha@nescode.com>
2018-11-25 21:43:49 +0530

    [Update]assets

--- assets/urls.py
+++ assets/urls.py
@@ -3,8 +3,19 @@ from . import views
 
 urlpatterns = [
     path('', views.electronicgadgets_list, name='electronicgadgets_list'),
+    path('electronicgadgets/<int:pk>/', views.electronicgadgets_detail, name='electronicgadgets_detail'),
+
     path('', views.cars_list, name='cars_list'),
+    path('kitchen/<int:pk>/', views.kitchen_detail, name='kitchen_detail'),
+
+
     path('', views.furnitures_list, name='furnitures_list'),
+    path('furnitures/<int:pk>/', views.furnitures_detail, name='furnitures_detail'),
+
     path('', views.plasticmaterials_list, name='plasticmaterials_list'),
+    path('plasticmaterials/<int:pk>/', views.plasticmaterials_detail, name='plasticmaterials_detail'),
+
     path('', views.kitchen_list, name='kitchen_list'),
+    path('cars/<int:pk>/', views.cars_detail, name='cars_detail'),
+
 ]
--- assets/views.py
+++ assets/views.py
@@ -1,25 +1,49 @@
-from django.shortcuts import render
-# from django.utils import timezone
-# from .models import Electronicgadgets
-# from .models import Kitchen
-# from .models import Furnitures
-# from .models import Plasticmaterials
-# from .models import Cars
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Electronicgadgets
+from .models import Kitchen
+from .models import Furnitures
+from .models import Plasticmaterials
+from .models import Cars
 
 def electronicgadgets_list(request):
-    # electronicgadgets.objects.filter(launch_date__lte=timezone.now()).order_by('launch_date')
-    return render(request, 'assets/electronicgadgets_list.html', {})
+    Electronicgadgets.objects.filter(launch_date__lte=timezone.now()).order_by('launch_date')
+    return render(request, 'assets/electronicgadgets_list.html', {'Electronicgadgets': Electronicgadgets})
+
+def electronicgadgets_detail(request, pk):
+    Electronicgadgets = get_object_or_404(Electronicgadgets, pk=pk)
+    return render(request, 'assets/electronicgadgets_detail.html', {'Electronicgadgets': Electronicgadgets})
 
 
-def cars_list(request):
-    return render(request, 'assets/cars_list.html', {})
+def kitchen_list(request):
+    Kitchen.objects.filter(expiry_date__lte=timezone.now()).order_by('manufactured_date')
+    return render(request, 'assets/kitchen_list.html', {'Kitchen': Kitchen})
+
+def kitchen_detail(request, pk):
+    Kitchen = get_object_or_404(Kitchen, pk=pk)
+    return render(request, 'assets/kitchen_detail.html', {'Kitchen': Kitchen})
 
 
 def furnitures_list(request):
-    return render(request, 'assets/furnitures_list.html', {})
+    Furnitures.objects.order_by('prize')
+    return render(request, 'assets/furnitures_list.html', {'Furnitures': Furnitures})
+
+def furnitures_detail(request, pk):
+    Furnitures = get_object_or_404(Post, pk=pk)
+    return render(request, 'assets/furnitures_detail.html', {'Furnitures': Furnitures})
 
 def plasticmaterials_list(request):
-    return render(request, 'assets/plasticmaterials_list.html', {})
+    Plasticmaterials.objects.order_by('prize')
+    return render(request, 'assets/plasticmaterials_list.html', {'Plasticmaterials': Plasticmaterials})
 
-def kitchen_list(request):
-    return render(request, 'assets/kitchen_list.html', {})
+def plasticmaterials_detail(request, pk):
+    Plasticmaterials = get_object_or_404(Plasticmaterials, pk=pk)
+    return render(request, 'assets/plasticmaterials_detail.html', {'Plasticmaterials': Plasticmaterials})
+
+def cars_list(request):
+    Cars.objects.order_by('launch_date')
+    return render(request, 'assets/cars_list.html', {'Cars': Cars})
+
+def cars_detail(request, pk):
+    Cars = get_object_or_404(Cars, pk=pk)
+    return render(request, 'assets/cars_detail.html', {'Cars': Cars})
